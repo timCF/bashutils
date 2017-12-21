0cd560db1b088dba44dbb88e9d32d5847f57b03b
Tkachuk Ilya <tim@Tkachuks-iMac.local>
2017-12-21 19:51:44 +0200

    update programs list

--- macmini.sh
+++ macmini.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 #
-# 1) install XCode from AppStore
-# 2) it's better to execute these 2 commands manually
+# execute 3 commands manually in terminal and enjoy!!!
+#
 #       xcode-select --install
 #       /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# 3) enjoy !!!
+#       git clone https://github.com/timCF/bashutils.git && cd ./bashutils && ./macmini.sh
 #
 brew update
 brew cask install iterm2 google-chrome atom amethyst slack skype java libreoffice thunderbird puush xquartz android-studio
-brew install wget curl elixir node python openssl nginx ruby protobuf fop wine homebrew/versions/mysql56 watchman
+brew install wget curl elixir node python openssl nginx ruby protobuf fop wine homebrew/versions/mysql56 postgres watchman
 brew tap homebrew/dupes
 brew install grep libtool automake autoconf
 mix local.hex --force
 mix local.rebar --force
-apm install language-elixir autocomplete-elixir language-protobuf language-sass language-erlang
+apm install language-elixir autocomplete-elixir language-protobuf language-sass language-erlang language-elm
 npm cache clean
 npm install npm -g
-npm install brunch bower react-native-cli -g
+npm install brunch bower react-native-cli elm elchemy -g
 sudo chown -R $(whoami) /Library/Ruby/Gems/2.0.0
 gem install sass jekyll
-pip install csv2xlsx
