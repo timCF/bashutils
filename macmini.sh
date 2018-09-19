478fbe17a83004541e72c6b4022eb43a21ae3b77
tim2CF <tim4CF@gmail.com>
2018-09-19 21:02:21 +0300

    rm brew dupes

--- macmini.sh
+++ macmini.sh
@@ -11,10 +11,8 @@ set -e
 
 brew update
 brew cask install iterm2 google-chrome atom amethyst spectacle slack skype telegram whatsapp java libreoffice xquartz
-brew install wget curl elixir node python openssl ruby protobuf fop wine postgres watchman
-brew tap homebrew/dupes
-brew install grep libtool automake autoconf
+brew install wget curl elixir node python openssl ruby protobuf fop wine postgres watchman grep libtool automake autoconf
 mix local.hex --force
 mix local.rebar --force
-apm install language-elixir autocomplete-elixir language-protobuf language-erlang language-elm
+apm install language-elixir autocomplete-elixir language-erlang language-protobuf language-elm
 gem install bundler jekyll
