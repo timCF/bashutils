9691ac25f9ad37f69fb36bfc0390b7dd23a27375
timcf <timcf@89-235-245-121.saturn.infonet.ee>
2019-04-28 15:47:47 +0300

    fix crf

--- compress-video.sh
+++ compress-video.sh
@@ -11,4 +11,4 @@ fi
 VIDEO_INPUT="$1"
 VIDEO_OUTPUT="$2"
 
-ffmpeg -i "$VIDEO_INPUT" -c:v libx264 -profile:v main -vf format=yuv420p -c:a aac -movflags +faststart -crf 28 "$VIDEO_OUTPUT"
+ffmpeg -i "$VIDEO_INPUT" -c:v libx264 -profile:v main -vf format=yuv420p -c:a aac -movflags +faststart -crf 30 "$VIDEO_OUTPUT"
